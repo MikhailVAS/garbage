70b62c19d4b8839c00258b85c12aed21e74837c7
Mikhail VAS <mikhailvas@gmail.com>
2017-12-26 22:44:25 +0300

    Add gamma correction and not damage files

---
+++ BNTU/laba_Gamma/SolarizeImage/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace SolarizeImage
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pic_original = new System.Windows.Forms.PictureBox();
+            this.pic_processed = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_original)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_processed)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pic_original
+            // 
+            this.pic_original.Location = new System.Drawing.Point(16, 15);
+            this.pic_original.Margin = new System.Windows.Forms.Padding(4);
+            this.pic_original.Name = "pic_original";
+            this.pic_original.Size = new System.Drawing.Size(399, 372);
+            this.pic_original.TabIndex = 0;
+            this.pic_original.TabStop = false;
+            // 
+            // pic_processed
+            // 
+            this.pic_processed.Location = new System.Drawing.Point(463, 15);
+            this.pic_processed.Margin = new System.Windows.Forms.Padding(4);
+            this.pic_processed.Name = "pic_processed";
+            this.pic_processed.Size = new System.Drawing.Size(399, 372);
+            this.pic_processed.TabIndex = 1;
+            this.pic_processed.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(32, 409);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(213, 55);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(355, 409);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(881, 479);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pic_processed);
+            this.Controls.Add(this.pic_original);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pic_original)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_processed)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pic_original;
+        private System.Windows.Forms.PictureBox pic_processed;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ BNTU/laba_Gamma/SolarizeImage/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace SolarizeImage
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Image img = Image.FromFile(@"d:\laba_Gamma\Pic.jpg", false);
+            Bitmap bit_map = new Bitmap(img);
+            pic_original.Image = img;
+
+            String user_median = textBox1.Text; // Получение введеного значения в компонент
+            double user_median_value = Convert.ToDouble(user_median);
+
+            if (user_median_value > 255) // Защита от дурака
+            {
+                user_median_value = 0.9;
+            }
+            else if (user_median_value > 1)
+            {
+                user_median_value = user_median_value / 255;
+            }
+            
+            else if (user_median_value < 0)
+            {
+                user_median_value = 0.001;
+            }
+            
+            pic_processed.Image = effect(bit_map, user_median_value);
+        }
+
+
+        private double[] get_values(double[] arr, int size) {
+
+            double[] new_arr = new double[arr.Length];
+            Array.Copy(arr, 0, new_arr, 0, arr.Length); // Клонирование массива 
+
+            double[] result = new double[3];
+            int median_index = (int)(size / 2); // Получение индекса медианного значения 
+            Array.Sort(new_arr);
+            result[0] = new_arr[0];  // Lmin
+            result[1] = new_arr[median_index];// Median
+            result[2] = new_arr[size-1];// Lmax
+
+            return result;
+
+        }
+
+
+        private double calculate_gamma(double user_median, double image_median, double image_min, double image_max)
+        {
+
+            return Math.Log(image_median, ((user_median - image_min) / (image_max - image_min)));// Вычисление гаммы
+        }
+
+
+        private Bitmap effect(Bitmap Image, double user_median)
+        {
+            int Height = Image.Height;
+            int Width = Image.Width;
+
+            Bitmap result = Image;
+
+            double[] pixels_values = new double[Height * Width];
+            double[] values = new double[3];
+            double min, max, median;
+
+            for (int i = 0; i < Height; i++)
+            {
+                for (int j = 0; j < Width; j++)
+                {
+                    Color Pixel = Image.GetPixel(j, i);
+                    Byte value = Pixel.R;
+                    pixels_values[i * Width + j] = ((((double)(value)))/255); // Нормализация значения пикселя 
+                }
+            }
+            
+            values = get_values(pixels_values, Width * Height);
+            min = values[0];
+            median = values[1];
+            max = values[2];
+            double gamma = calculate_gamma(user_median, median, min, max);
+
+            for (int i = 0; i < Width * Height; i++)
+            {
+                pixels_values[i] = (Math.Pow(pixels_values[i], gamma)) * 255; // Расчет результирующего значения пикселей
+            }
+
+            for (int i = 0; i < Height; i++)
+            {
+                for (int j = 0; j < Width; j++)
+                {
+                    Byte color = (Byte)(pixels_values[i * Width + j]);
+                    Color new_pixel = Color.FromArgb(255, color, color, color);
+                    result.SetPixel(j, i, new_pixel);
+                }
+
+            }
+
+            return result;
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e) //Загрузка изображения в компаненты
+        {
+            Image img = Image.FromFile(@"d:\laba_Gamma\Pic.jpg", false);
+            Bitmap bit_map = new Bitmap(img);
+            pic_original.Image = img;
+            pic_processed.Image = bit_map;
+        }
+    }
+}
