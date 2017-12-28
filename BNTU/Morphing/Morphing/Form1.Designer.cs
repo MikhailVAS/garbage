cc3911a3d1ae1c32aee058ba25a1532cf5e48bd0
Mikhail VAS <mikhailvas@gmail.com>
2017-12-28 23:31:39 +0300

    Morphing

---
+++ BNTU/Morphing/Morphing/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace Morphing
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pic_processed = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pic_original1 = new System.Windows.Forms.PictureBox();
+            this.pic_original2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_processed)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_original1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_original2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pic_processed
+            // 
+            this.pic_processed.Location = new System.Drawing.Point(511, 12);
+            this.pic_processed.Name = "pic_processed";
+            this.pic_processed.Size = new System.Drawing.Size(253, 366);
+            this.pic_processed.TabIndex = 0;
+            this.pic_processed.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 384);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(752, 52);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pic_original1
+            // 
+            this.pic_original1.Location = new System.Drawing.Point(6, 12);
+            this.pic_original1.Name = "pic_original1";
+            this.pic_original1.Size = new System.Drawing.Size(250, 366);
+            this.pic_original1.TabIndex = 2;
+            this.pic_original1.TabStop = false;
+            // 
+            // pic_original2
+            // 
+            this.pic_original2.Location = new System.Drawing.Point(263, 12);
+            this.pic_original2.Name = "pic_original2";
+            this.pic_original2.Size = new System.Drawing.Size(242, 366);
+            this.pic_original2.TabIndex = 3;
+            this.pic_original2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(777, 438);
+            this.Controls.Add(this.pic_original2);
+            this.Controls.Add(this.pic_original1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pic_processed);
+            this.Name = "Form1";
+            this.Text = "Морфинг изображений";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pic_processed)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_original1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_original2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pic_processed;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pic_original1;
+        private System.Windows.Forms.PictureBox pic_original2;
+    }
+}
+
---
+++ BNTU/Morphing/Morphing/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Morphing
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Image img = Image.FromFile(@"d:\Git\garbage\BNTU\Morphing\Morphing\IronMan.jpg");
+            Bitmap bit_map = new Bitmap(img);
+            pic_original1.Image = img;
+            pic_processed.Image = effect(bit_map, 0.3);
+        }
+
+        private Byte transformation(Byte color, double p)
+        {
+            Byte result;
+
+            //Gamma correction
+            result = (byte)(Math.Pow(((double)color / 255), p) * 255);
+            return result;
+        }
+
+
+        private Bitmap effect(Bitmap Image, double p)
+        {
+            int Height = Image.Height;
+            int Width = Image.Width;
+
+            Bitmap result = Image;
+
+            for (int i = 0; i < Height; i++)
+            {
+                for (int j = 0; j < Width; j++)
+                {
+                    Color Pixel = Image.GetPixel(j, i);
+                    Byte r = Pixel.R;
+                    Byte g = Pixel.G;
+                    Byte b = Pixel.B;
+
+                    Byte new_r, new_g, new_b;
+                    new_r = transformation(r, p);
+                    new_g = transformation(g, p);
+                    new_b = transformation(b, p);
+                    Color new_pixel = Color.FromArgb(255, new_r, new_g, new_b);
+                    result.SetPixel(j, i, new_pixel);
+                }
+
+            }
+            return result;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Image img1 = Image.FromFile(@"d:\Git\garbage\BNTU\Morphing\Morphing\IronMan.jpg");
+            Bitmap bit_map1 = new Bitmap(img1);
+            pic_original1.Image = img1;
+            Image img2 = Image.FromFile(@"d:\Git\garbage\BNTU\Morphing\Morphing\Joke.jpg");
+            Bitmap bit_map2 = new Bitmap(img2);
+            pic_original2.Image = img2;
+            pic_processed.Image = bit_map1;
+        }
+    }
+}
